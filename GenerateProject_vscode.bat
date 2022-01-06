50c454e0f5572c96369517d9dea72adbb4350c05
sky <sky@163.com>
2022-01-06 16:17:46 +0800

    1

--- GenerateProject_vscode.bat
+++ GenerateProject_vscode.bat
@@ -1,2 +1,2 @@
-C:/UnrealEngine/Engine/Binaries/DotNET/UnrealBuildTool.exe -projectfiles -project="D:\Project_Test\Prj_Test\Prj_Test.uproject" -game -progress -engine -vscode
+C:/UnrealEngine/Engine/Binaries/DotNET/UnrealBuildTool.exe -projectfiles -project="D:\Project_Test\TestAssetManager\Prj_Test.uproject" -game -progress -engine -vscode
 pause
